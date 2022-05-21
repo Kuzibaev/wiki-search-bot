0ca9f3f1a5e5a14848abae267212548e5e346275
Turonbek Kuzibaev <qoziboyevturonebek@gmail.com>
2022-05-21 11:07:32 +0500

    README

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
+import logging
+
 import requests
-from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler
-from telegram.update import Update
-import settings
+from telegram.ext import Updater, CommandHandler, CallbackContext, \
+    MessageHandler
 from telegram.ext.filters import Filters
+from telegram.update import Update
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG)
+
+import settings
+
 updater = Updater(token=settings.TELEGRAM_TOKEN)
 
 
 def start(update: Update, context: CallbackContext):
-    update.message.\
-        reply_text("Assalomu Aleykum. Vikipediadan maʼlumot qidiruvchi botga Xush kelibsiz. "
-                   "Qidirish uchun /search va so‘rovingizni qoldiring. "
-                   "Misol uchun: /search Amir Temur")
+    update.message \
+        .reply_text('Assalomu alaykum! Vikipediadan maʼlumot qidiruvchi '
+                    'botga hush kelibsiz! Biron nima izlash uchun /search '
+                    'va so‘rovingizni yozing. Misol uchun /search Amir Temur')
 
 
 def search(update: Update, context: CallbackContext):
     args = context.args
+
+    logging.info('checking args length')
+
     if len(args) == 0:
-        update.message.reply_text('Hech bo‘lmaganda biron nima kiriting Masalan: /search Amir Temur')
-        pass
+        update.message \
+            .reply_text('Hech bo‘lmasa, nimadir kiriting. Misol uchun '
+                        '/search Amir Temur')
     else:
         search_text = ' '.join(args)
+        logging.info('sending request to Wikipedia API')
         response = requests.get('https://uz.wikipedia.org/w/api.php', {
             'action': 'opensearch',
             'search': search_text,
@@ -27,13 +41,17 @@ def search(update: Update, context: CallbackContext):
             'namespace': 0,
             'format': 'json',
         })
+
+        logging.info('result from Wikipedia API')
         result = response.json()
         link = result[3]
-        if len(link):
-            update.message.reply_text("Sizning so'rovingiz bo'yicha ilova: " + link[0])
 
+        if len(link):
+            update.message \
+                .reply_text('Sizning so‘rovingiz bo‘yicha havola: ' + link[0])
         else:
-            update.message.reply_text("Sizning so'rovingiz bo'yicha hech nima topilmadi.")
+            update.message \
+                .reply_text('Sizning so‘rovingiz bo‘yicha hech nima yo‘q')
 
 
 dispatcher = updater.dispatcher
@@ -41,6 +59,5 @@ dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(CommandHandler('search', search))
 dispatcher.add_handler(MessageHandler(Filters.all, start))
 
-print("Bot starting...")
 updater.start_polling()
 updater.idle()
